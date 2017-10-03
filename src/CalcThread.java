c24909e8d374efaab340aa24846f6db32c2258ce
Everton Cavalcante <evertonranielly@gmail.com>
2017-10-02 23:44:19 -0300

    Implementação da classe thread que realiza o cálculo de um termo da série

--- src/CalcThread.java
+++ src/CalcThread.java
@@ -1,13 +1,41 @@
+/*
+ ===============================================================================
+ ARQUIVO............: CalcThread.java
+ DESCRICAO..........: Implementacao do calculo de um termo da serie de Leibniz 
+ 					  por uma thread
+ AUTOR..............: Everton Cavalcante (everton@dimap.ufrn.br)
+ CRIADO EM..........: 02/10/2017
+ MODIFICADO EM......: 02/10/2017
+ ===============================================================================
+*/
+
 import java.util.List;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
+/**
+ * Implementacao do calculo de um termo da serie de Leibniz por uma 
+ * <em>thread</em>
+ */
 public class CalcThread extends Thread {
+	/** <em>i</em>-esimo termo da serie a ser calculado */
 	private int indice;
+	
+	/** Lista de resultados parciais computados */
 	private List<Double> termos;
 
+	/** 
+	 * Referencia a barreira que ira realizar a sincronizacao
+	 * @see java.util.concurrent.CyclicBarrier
+	 */
 	private CyclicBarrier barreira;
 	
+	/**
+	 * Construtor parametrizado
+	 * @param indice Indice do termo a ser calculado
+	 * @param termos Referencia a lista na qual o resultado computado sera armazenado 
+	 * @param barreira Referencia a barreira para sincronizacao
+	 */
 	public CalcThread(int indice, List<Double> termos, CyclicBarrier barreira) {
 		this.indice = indice;
 		this.termos = termos;
@@ -20,6 +48,8 @@ public class CalcThread extends Thread {
 		termos.add(termo);
 		
 		try {
+			/* Apos a conclusao do calculo do termo, a thread e suspensa
+			 * na barreira esperando pelas demais threads */ 
 			barreira.await();
 		} catch (InterruptedException | BrokenBarrierException e) {
 			e.printStackTrace();
