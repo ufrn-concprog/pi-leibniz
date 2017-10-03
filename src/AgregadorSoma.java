45cda31a1d59dca5656e6137f41910ea5efaa906
Everton Cavalcante <evertonranielly@gmail.com>
2017-10-02 23:48:15 -0300

    Implementação de classe de para agregação (soma) dos valores computados

--- src/AgregadorSoma.java
+++ src/AgregadorSoma.java
@@ -1,8 +1,29 @@
+/*
+ ===============================================================================
+ ARQUIVO............: AgregadorSoma.java
+ DESCRICAO..........: Implementacao de objeto Runnable, executado como uma thread, 
+ 					  para a soma dos valores contidos em uma lista
+ AUTOR..............: Everton Cavalcante (everton@dimap.ufrn.br)
+ CRIADO EM..........: 02/10/2017
+ MODIFICADO EM......: 02/10/2017
+ ===============================================================================
+*/
+
 import java.util.List;
 
+/**
+ * Implementacao de objeto <code>Runnable</code>, 
+ * executado como uma <em>thread</em>, 
+ * para a soma dos valores contidos em uma lista
+ */
 public class AgregadorSoma implements Runnable {
+	/** Lista de valores a serem somados */
 	private List<Double> termos;
 	
+	/**
+	 * Construtor parametrizado
+	 * @param termos Lista de valores
+	 */
 	public AgregadorSoma(List<Double> termos) {
 		this.termos = termos;
 	}
